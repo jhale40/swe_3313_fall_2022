b442c9759ef5a170a025c2fe2a288789844ce305
jhale40 <jhale40@students.kennesaw.edu>
2022-11-30 20:10:50 -0500

    Added the rest of the Forms

---
+++ Source/CoffeePointOfSale/Forms/FormPayment.cs
@@ -0,0 +1,27 @@
+﻿using CoffeePointOfSale.Configuration;
+using CoffeePointOfSale.Services.Customer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeePointOfSale.Forms
+{
+    public partial class FormPayment : Base.FormNoCloseBase
+    {
+        private IAppSettings _appSettings;
+        private ICustomerService _customerService;
+
+        public FormPayment(IAppSettings appSettings, ICustomerService customerService) : base(appSettings)
+        {
+            InitializeComponent();
+            _appSettings = appSettings;
+            _customerService = customerService;
+        }
+    }
+}
--- Source/CoffeePointOfSale/Forms/FormReceipt.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormReceipt.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CoffeePointOfSale.Forms
 {
-    partial class FormAddCustomer
+    partial class FormReceipt
     {
         /// <summary>
         /// Required designer variable.
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "FormAddCustomer";
+            this.Text = "FormReceipt";
         }
 
         #endregion
