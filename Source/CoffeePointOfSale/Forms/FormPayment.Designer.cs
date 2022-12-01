daeeee259ad26782049cfa8991e5b7304e07b779
jhale40 <jhale40@students.kennesaw.edu>
2022-12-01 17:31:05 -0500

    d

--- Source/CoffeePointOfSale/Forms/FormPayment.Designer.cs
+++ Source/CoffeePointOfSale/Forms/FormPayment.Designer.cs
@@ -45,6 +45,7 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button1.Location = new System.Drawing.Point(608, 197);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -54,6 +55,7 @@
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.button2.Location = new System.Drawing.Point(608, 277);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
